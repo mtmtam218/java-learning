5db07a84169e6ebcf407733d84d997d25af6e4c9
mtmtam218 <104251862+mtmtam218@users.noreply.github.com>
2025-10-24 23:46:37 +0900

    Create BooleanLiteral.java
    BooleanLiteral.javaを追加

---
+++ 01_literals/BooleanLiteral.java
@@ -0,0 +1,11 @@
+//真偽値リテラル
+
+public class BooleanLiteral {
+  public static void main(String[] args){
+    boolean flag1 = true;
+    boolean flag2 = false;
+
+    System.out.println(flag1); //true
+    System.out.println(flag2); //false
+  }
+}
