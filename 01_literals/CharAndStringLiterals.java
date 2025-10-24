f93a6bf18eaf05e3979e3a871cd894716f326286
mtmtam218 <104251862+mtmtam218@users.noreply.github.com>
2025-10-24 23:43:32 +0900

    Create CharAndStringLiterals.java
    CharAndStringLiterals.javaを追加

---
+++ 01_literals/CharAndStringLiterals.java
@@ -0,0 +1,11 @@
+//文字と文字列リテラル
+
+public class CharAndStringLiterals {
+  public static void main (String[] args){
+    char c = 'a'; //文字リテラル
+    String s = "abc"; //文字列リテラル
+
+    System.out.println(c);
+   System.out.println(s); 
+  }
+}
